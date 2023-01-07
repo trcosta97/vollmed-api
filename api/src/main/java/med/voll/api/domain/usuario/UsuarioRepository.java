41e316decb662c9107d9fdd50d299ad72d157a6a
Thiago Ribeiro <trcosta97@hotmail.com>
2023-01-07 10:36:51 -0300

    spring security

---
+++ api/src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package med.voll.api.domain.usuario;
+
+import med.voll.api.domain.medico.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
